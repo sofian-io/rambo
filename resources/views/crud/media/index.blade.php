46103594467be146642ebb034cd9f207320e7e32
AngryMoustache <sander.vandamme11@gmail.com>
2021-05-07 16:48:27 +0200

    Add new button HABTM

--- resources/views/crud/media/index.blade.php
+++ resources/views/crud/media/index.blade.php
@@ -1,7 +1,5 @@
 @extends('rambo::layouts.admin')
 
 @section('content')
-    <div class="border p-5 pt-3 rounded-lg bg-white shadow">
-        <livewire:rambo-crud-media-table :resource="$resource" />
-    </div>
+    <livewire:rambo-crud-media-table :resource="$resource" />
 @endsection
