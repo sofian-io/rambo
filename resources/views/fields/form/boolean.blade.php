2223e597b7b2b8321f5abd80ae65d2717dec819f
Sofian Mourabit <sofian@codedor.be>
2021-09-09 09:05:06 +0200

    Tweak for bool resource label

--- resources/views/fields/form/boolean.blade.php
+++ resources/views/fields/form/boolean.blade.php
@@ -1,5 +1,6 @@
 <div class="crud-form-field">
     <div class="crud-form-field-label">
+        <x-rambo::crud.form.label :field="$field" />
     </div>
 
     <div class="crud-form-field-input crud-form-field-checkbox">
@@ -10,8 +11,6 @@
             wire:model="{{ $field->getBindingName() }}"
             @if ($field->readonly || $field->disabled) disabled @endif
         >
-
-        <x-rambo::crud.form.label :field="$field" />
 
         <x-rambo::crud.form.error :field="$field" />
     </div>
